091ee551798a7b1d85f0b68a0e52f7bb5f1498e2
Prajwal Adhikari <prajwalad101@gmail.com>
2022-06-10 22:25:28 +0545

    feat(style): make overall layout more readable

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from 'next';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { io, Socket } from 'socket.io-client';
 import { SERVER_URL } from '../config/default';
 import {
@@ -15,37 +15,44 @@ const Home: NextPage = () => {
   const [messageInput, setMessageInput] = useState('');
   const [messages, setMessages] = useState<string[]>([]);
 
+  const messageEndRef = useRef<HTMLDivElement>(null);
+
   useEffect(() => {
     socket.on(EVENTS['chat message'], (message) => {
-      setMessages([message, ...messages]);
+      setMessages([...messages, message]);
     });
+    messageEndRef.current?.scrollIntoView({ behavior: 'smooth' });
   }, [messages]);
 
-  const sendMessage = () => {
+  const sendMessage = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
     socket.emit(EVENTS['chat message'], messageInput);
     setMessageInput('');
   };
 
   return (
-    <div className="mx-auto items-center flex justify-center  h-[100vh]">
-      <div>
+    <div className="h-[100vh] px-10">
+      <div className="h-[80vh] overflow-y-auto">
         {messages.map((message, i) => (
           <p key={i}>{message}</p>
         ))}
+        <div ref={messageEndRef} />
       </div>
-      <div>
-        <input
-          className="border-2 p-2 rounded-md"
-          type="text"
-          onChange={(e) => setMessageInput(e.target.value)}
-          value={messageInput}
-        />
-        <button
-          onClick={sendMessage}
-          className="px-7 py-2 rounded-md ml-4 bg-blue-600 text-white"
-        >
-          Send
-        </button>
+      <div className="w-[80%]">
+        <form onSubmit={sendMessage} className="mb-10 flex justify-between">
+          <input
+            className="w-full rounded-sm border-2 p-2"
+            type="text"
+            onChange={(e) => setMessageInput(e.target.value)}
+            value={messageInput}
+          />
+          <button
+            type="submit"
+            className="ml-4 rounded-sm bg-blue-600 px-7 py-2 text-white"
+          >
+            Send
+          </button>
+        </form>
       </div>
     </div>
   );
