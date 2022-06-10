5f399946a994f66c947ab59b04d7c5555f0412cd
Prajwal Adhikari <prajwalad101@gmail.com>
2022-06-10 17:26:17 +0545

    feat: implement socket connection from the client

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,4 +1,9 @@
 import type { NextPage } from 'next';
+import { io } from 'socket.io-client';
+import { SERVER_URL } from '../config/default';
+
+const url = SERVER_URL;
+io(url);
 
 const Home: NextPage = () => {
   return <div>Hello world</div>;
