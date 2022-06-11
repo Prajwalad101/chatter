900c755970c7b7aee1b858a0ffe119a7325b7fab
Prajwal Adhikari <prajwalad101@gmail.com>
2022-06-11 18:50:05 +0545

    perf: avoid registering multiple handler functions for incoming message events

--- client/components/MessageContainer/TypingUser.tsx
+++ client/components/MessageContainer/TypingUser.tsx
@@ -1,17 +1,7 @@
 import { useSocket } from '../../context/socket.context';
 
 function TypingUser() {
-  const { typingUser, setTypingUser } = useSocket();
-
-  // useEffect(() => {
-  //   // TODO: if there is a typing user, reset the user every 3 seconds
-  //   if (typingUser) {
-  //     setInterval(() => {
-  //       console.log('Set interval function called');
-  //       setTypingUser('');
-  //     }, 3000);
-  //   }
-  // }, [typingUser, setTypingUser]);
+  const { typingUser } = useSocket();
 
   return <div>{typingUser && <p>{typingUser} is typing</p>}</div>;
 }
